2169cdc37e07f9b6035742460866d6005f3e794b
ZiZiZiEtaPabeda1945 <zaluzecurij@gmail.com>
2025-04-15 17:53:37 +0300

    impl and mapper added to placesService

--- placesService/src/main/java/org/example/placesservice/controllers/PlaceController.java
+++ placesService/src/main/java/org/example/placesservice/controllers/PlaceController.java
@@ -2,39 +2,29 @@ package org.example.placesservice.controllers;
 
 import org.example.placesservice.dto.PlaceDTO;
 import org.example.placesservice.models.PlaceModel;
-import org.example.placesservice.services.PlaceService;
+import org.example.placesservice.services.impl.PlaceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/places")
 public class PlaceController {
 
-    private final PlaceService placeService;
+    private final PlaceServiceImpl placeService;
 
     @Autowired
-    public PlaceController(PlaceService placeService) {
+    public PlaceController(PlaceServiceImpl placeService) {
         this.placeService = placeService;
     }
 
     @GetMapping
     public ResponseEntity<List<PlaceDTO>> getPlace(@RequestParam String placeName) {
         try {
-            List<PlaceModel> places = placeService.getPlaceByPlaceName(placeName);
-            List<PlaceDTO> response = places.stream()
-                    .map(placeModel -> new PlaceDTO(placeModel.getPlaceName(),
-                            placeModel.getCity(),
-                            placeModel.getCounty(),
-                            placeModel.getStreet(),
-                            placeModel.getHouseNumber(),
-                            placeModel.getCoordinates()))
-                    .collect(Collectors.toList());
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok(placeService.getPlaceByPlaceName(placeName));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -43,16 +33,7 @@ public class PlaceController {
     @GetMapping("/{city}")
     public ResponseEntity<List<PlaceDTO>> getCities(@PathVariable String city) {
         try {
-            List<PlaceModel> places = placeService.getAllPlacesByCity(city);
-            List<PlaceDTO> response = places.stream()
-                    .map(placeModel -> new PlaceDTO(placeModel.getPlaceName(),
-                            placeModel.getCity(),
-                            placeModel.getCounty(),
-                            placeModel.getStreet(),
-                            placeModel.getHouseNumber(),
-                            placeModel.getCoordinates()))
-                    .collect(Collectors.toList());
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok(placeService.getAllPlacesByCity(city));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
@@ -61,16 +42,7 @@ public class PlaceController {
     @GetMapping("/getAllPlaces")
     public ResponseEntity<List<PlaceDTO>> getAllCities() {
         try {
-            List<PlaceModel> places = placeService.getAllPlaces();
-            List<PlaceDTO> response = places.stream()
-                    .map(placeModel -> new PlaceDTO(placeModel.getPlaceName(),
-                            placeModel.getCity(),
-                            placeModel.getCounty(),
-                            placeModel.getStreet(),
-                            placeModel.getHouseNumber(),
-                            placeModel.getCoordinates()))
-                    .collect(Collectors.toList());
-            return ResponseEntity.ok(response);
+            return ResponseEntity.ok(placeService.getAllPlaces());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
