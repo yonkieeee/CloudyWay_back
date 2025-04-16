f2deaa63333f718bc77b19b5fd67bab1e13643c6
ZiZiZiEtaPabeda1945 <zaluzecurij@gmail.com>
2025-04-16 21:20:25 +0300

    .jar built

---
+++ placesService/src/test/java/org/example/placesservice/PlacesServiceApplicationService.java
@@ -0,0 +1,20 @@
+package org.example.placesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+@SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:postgresql://localhost:5432/test_db_placeholder",
+        "spring.datasource.username=test_user_placeholder",
+        "spring.datasource.password=test_password_placeholder",
+        "spring.liquibase.enabled=false"
+})
+class PlacesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
